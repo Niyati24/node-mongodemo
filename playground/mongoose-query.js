37f4c761a48658b8a5d1e5a5dbb12e305685e65e
unknown <govila.niyati@gmail.com>
2018-05-28 16:33:11 +0200

    adding /get id

--- playground/mongoose-query.js
+++ playground/mongoose-query.js
@@ -2,7 +2,7 @@ var mongoose = require('./../server/db/mongoose');
 var {Todo} = require('./../server/models/todo');
 var {ObjectID} = require('mongodb');
 
-var id='6b0bfcad37808f6c2f82ca0d';
+var id='5b0bfcad37808f6c2f82ca0d';
 if(ObjectID.isValid(id))
 {
 console.log('Is valid');
--- server/server.js
+++ server/server.js
@@ -32,6 +32,7 @@ module.exports={app};
 */
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -60,6 +61,23 @@ app.get('/todos',(req,res)=>{
       res.status(400).send(e);
    });
   
+});
+app.get('/todos/:id',(req,res)=>{
+var id= req.params.id;
+  if(!ObjectID.isValid(id)){
+    res.status(404).send();
+  }
+  Todo.findById(id).then((todo)=>{
+    if(todo){
+      res.status(200).send(todo);
+    }
+      res.status(404).send();
+      
+    
+  }
+  ,(e)=>{ res.status(404).send();
+  })
+
 });
 
 app.listen(3000, () => {
