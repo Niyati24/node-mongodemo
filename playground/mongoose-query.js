98410c107cafc8012dedc3bdd7fa0d807b76a5a8
unknown <govila.niyati@gmail.com>
2018-05-28 16:04:03 +0200

    adding queries

---
+++ playground/mongoose-query.js
@@ -0,0 +1,29 @@
+var mongoose = require('./../server/db/mongoose');
+var {Todo} = require('./../server/models/todo');
+var {ObjectID} = require('mongodb');
+
+var id='6b0bfcad37808f6c2f82ca0d';
+if(ObjectID.isValid(id))
+{
+console.log('Is valid');
+}
+
+Todo.find({
+    _id:id
+
+}).then((todos)=>{
+    console.log('Todods',todos);
+})
+
+Todo.findOne({
+    _id:id
+
+}).then((todos)=>{
+    console.log('Todods',todos);
+})
+
+
+Todo.findById(id).then((todos)=>{
+    console.log('Todods',todos);
+}).catch((e)=>console.log(e));
+//findOne
